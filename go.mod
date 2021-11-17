f4b9d7f077ea5e1b727f3de36e79765ebc4c3172
aopal <anuj.opal@shopify.com>
2021-11-17 12:18:48 -0500

    Initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module hammer
+
+go 1.16
+
+require (
+	golang.org/x/net v0.0.0-20210423184538-5f58ad60dda6
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
+)
---
+++ hammer.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"runtime"
+	"time"
+
+	"golang.org/x/net/http2"
+	"golang.org/x/sync/semaphore"
+)
+
+type hammerSettings struct {
+	concurrencyFactor int
+	client            *http.Client
+	delay             time.Duration
+	sem               *semaphore.Weighted
+	requestHeaders    http.Header
+	urls              []string
+	useHTTP2          bool
+}
+
+func getSettings() *hammerSettings {
+	settings := hammerSettings{
+		requestHeaders: make(http.Header),
+	}
+
+	// set flags
+	flag.IntVar(&settings.concurrencyFactor, "c", runtime.GOMAXPROCS(0), "Concurrency factor, number of requests to make concurrently. Defaults to the value of runtime.GOMAXPROCS(0)")
+	flag.DurationVar(&settings.delay, "d", 0, "Delay to wait after making a request")
+	flag.BoolVar(&settings.useHTTP2, "http2", false, "Use HTTP/2 for requests")
+	flag.Func("header", "Set a request header in the form \"header-name: header-value\" (multiple invocations allowed)", func(str string) error {
+
+		h := strings.Split(str, ":")
+		if len(h) != 2 {
+			return fmt.Errorf("Header must be in the form \"header-name: header-value\"")
+		}
+
+		settings.requestHeaders.Add(h[0], h[1])
+		return nil
+	})
+
+	// parse flags
+	flag.Parse()
+
+	// post process flags
+	settings.urls = flag.Args()
+
+	settings.sem = semaphore.NewWeighted(int64(settings.concurrencyFactor))
+	settings.client = &http.Client{}
+	if settings.useHTTP2 {
+		settings.client.Transport = &http2.Transport{}
+	}
+
+	return &settings
+}
+
+func logStats(n int, startTime time.Time) {
+	t := time.Now()
+	elapsed := t.Sub(startTime)
+	avgRate := float64(n) / float64(elapsed/time.Millisecond) * 1000.0 * 60.0
+
+	fmt.Printf("\rCompleted at least %d iterations in %v (average %0.2f iterations/minute)...", n, elapsed.Round(1000*time.Millisecond), avgRate)
+}
+
+// Make a request against a url
+func doRequest(settings *hammerSettings, i int) {
+	defer settings.sem.Release(1)
+
+	url := settings.urls[i%len(settings.urls)]
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	req.Close = false
+	req.Header = settings.requestHeaders.Clone()
+
+	res, err := settings.client.Do(req)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if res.StatusCode != 200 && res.StatusCode != 404 {
+		fmt.Println("\nReceived non 200 response:", res.StatusCode, url)
+	}
+
+	// read the response body
+	go func() {
+		io.Copy(ioutil.Discard, res.Body)
+		res.Body.Close()
+	}()
+
+	if settings.delay != 0 {
+		fmt.Println("sleeping")
+		time.Sleep(settings.delay)
+	}
+}
+
+func loadTest(settings *hammerSettings) {
+	fmt.Println("max concurrent requests:", settings.concurrencyFactor)
+	fmt.Println("max parallel goroutines:", runtime.GOMAXPROCS(0))
+
+	ctx := context.Background()
+	start := time.Now()
+
+	for i := 0; true; i++ {
+		if err := settings.sem.Acquire(ctx, 1); err != nil {
+			fmt.Printf("Failed to acquire semaphore: %v", err)
+			break
+		}
+
+		go doRequest(settings, i)
+
+		go logStats(i, start)
+	}
+}
+
+func main() {
+	settings := getSettings()
+	loadTest(settings)
+}
